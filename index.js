7f50e364f5e75d7584b11902743ab1883aac80d3
Hadeel Alharthi <hadeel.alharthi@hotmail.com>
2016-09-04 20:45:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list li');
+  console.log(lis);
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div');
+}
